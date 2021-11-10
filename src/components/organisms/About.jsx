563fcb4b597e727fc2ab44ff4ba11df91fc9ecaf
nathaneleven <nathan.martins@elevenrush.com>
2021-11-10 16:31:58 -0300

    Corrigido alinhamento About

--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -5,15 +5,9 @@ import { CheckIcon } from '@chakra-ui/icons'
 const About = () => {
     return (
         <>
-        <Stack w="100%" justify="center" flexDirection={["column","column","column","column","row"]}>
+        <Stack w="100%" justify="center" flexDirection={["column-reverse","column-reverse","column-reverse","column-reverse","row-reverse"]}>
 
-            <Flex w={["100%","100%","100%","100%","50%"]} justify="flex-start">
-                <AspectRatio w={["100%","100%","100%","100%","100%"]} mb={["10px","20px","30px","30px","50px"]} ratio={[3, 3, 3, 3, 6/4]} >
-                    <Image objectPosition={["0 -34px","0 -40px","0 -50px","0 -60px", "0px"]} borderRadius={{xl: "0 0 70px 0"}} src="/about-metronet-image.jpg" alt="About Image Metronet" />
-                </AspectRatio>
-            </Flex>
-
-                <Box pb={["25px","25px","25px","25px","0"]} w={["100%","100%","100%","100%","50%"]} m="auto">
+        <Box pb={["25px","25px","25px","25px","0"]} w={["100%","100%","100%","100%","50%"]} m="auto">
                 <Box p={["4","5","6","7","8"]}  w={["100%","100%","100%","100%","100%"]} m="auto">
                     <Heading mb="24px">
                     About MetroNet
@@ -25,6 +19,12 @@ const About = () => {
                         </Text>
                 </Box>
                 </Box>
+
+            <Flex mt="0 !important" w={["100%","100%","100%","100%","50%"]} justify="flex-start">
+                <AspectRatio w={["100%","100%","100%","100%","100%"]} mb={["10px","20px","30px","30px","50px"]} ratio={[3, 3, 3, 3, 6/4]} >
+                    <Image objectPosition={["0 -34px","0 -40px","0 -50px","0 -60px", "0px"]} borderRadius={{xl: "0 0 70px 0"}} src="/about-metronet-image.jpg" alt="About Image Metronet" />
+                </AspectRatio>
+            </Flex>
                 </Stack>
         </>
     )}
