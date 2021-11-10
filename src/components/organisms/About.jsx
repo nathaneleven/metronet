dc3ade3949d17290085ba33050f3390e1ac8e267
nathaneleven <nathan.martins@elevenrush.com>
2021-11-10 16:28:54 -0300

    COrreção de alinhamento, aind não consegui alinhar verticalmente o texto do About

--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
-import { Flex, Box, Heading, Text, AspectRatio } from '@chakra-ui/layout'
+import { Flex, Box, Heading, Text, AspectRatio, Stack, List, ListIcon, ListItem, Spacer } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
-
+import { CheckIcon } from '@chakra-ui/icons'
 const About = () => {
     return (
-<>
-    <Flex align="center" bgColor="#fff" direction={{base: "column", md: "row"}} pb="40px" >
+        <>
+        <Stack w="100%" justify="center" flexDirection={["column","column","column","column","row"]}>
 
-            <AspectRatio w="100%" ratio={{base:3, md:6/4 }} >
-                <Image borderRadius={{md: "0 0 70px 0"}} src="/about-metronet-image.jpg" alt="Hero Image Metronet" />
-            </AspectRatio>
-            
-            <Box m="25px">
-                <Heading mb="24px" >
-                About MetroNet 
-                </Heading>
-                <Text>
-                Metronet is a community-driven ISP (Internet Service Provider). The company supplies fiber internet, full-featured fiber phone, fiber IPTV, among other products, to their customers.<br/><br/>
+            <Flex w={["100%","100%","100%","100%","50%"]} justify="flex-start">
+                <AspectRatio w={["100%","100%","100%","100%","100%"]} mb={["10px","20px","30px","30px","50px"]} ratio={[3, 3, 3, 3, 6/4]} >
+                    <Image objectPosition={["0 -34px","0 -40px","0 -50px","0 -60px", "0px"]} borderRadius={{xl: "0 0 70px 0"}} src="/about-metronet-image.jpg" alt="About Image Metronet" />
+                </AspectRatio>
+            </Flex>
 
-                The ISP is serving over two million people in the US and is currently the eighth largest fiber-optic provider. Metronet is available in seven states, including Minnesota, Illinois, Iowa, Indiana, Michigan, Ohio, and Kentucky.  
-                </Text>
-            </Box>
+                <Box pb={["25px","25px","25px","25px","0"]} w={["100%","100%","100%","100%","50%"]} m="auto">
+                <Box p={["4","5","6","7","8"]}  w={["100%","100%","100%","100%","100%"]} m="auto">
+                    <Heading mb="24px">
+                    About MetroNet
+                    </Heading>
+                        <Text>
+                        Metronet is a community-driven ISP (Internet Service Provider). The company supplies fiber internet, full-featured fiber phone, fiber IPTV, among other products, to their customers.<br/><br/>
 
-    </Flex>
-</>
+                        The ISP is serving over two million people in the US and is currently the eighth largest fiber-optic provider. Metronet is available in seven states, including Minnesota, Illinois, Iowa, Indiana, Michigan, Ohio, and Kentucky.  
+                        </Text>
+                </Box>
+                </Box>
+                </Stack>
+        </>
     )}
 
 export default About
--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
         <>
         <Stack w="100%" justify="center" flexDirection={["column-reverse","column-reverse","column-reverse","column-reverse","row"]}>
         {/*Retirei um flex e adicionei um box para facilitar o alinhamento*/}
-        <Box h="100%" w={["100%","100%","100%","100%","50%"]} m="auto">
+        <Box pb={["25px","25px","25px","25px","0"]} w={["100%","100%","100%","100%","50%"]} m="auto">
         {/*a width vai ser a responsável pelo alinamento da div de texto*/}
-        <Box p={["4","5","6","7","8"]}  w={["100%","80%","80%","70%","75%"]} m="auto">
+        <Box p={["4","5","6","7","8"]}  w={["100%","100%","100%","100%","75%"]} m="auto">
             <Heading as="h1" mb="24px">
             MetroNet: 100% Fiber, TV and Phone for you
             </Heading>
@@ -30,11 +30,10 @@ const Hero = () => {
             </List>
         </Box>
         </Box>
-        <Spacer/>
         {/*correção de breakpoints*/}
-        <Flex w={["100%","100%","100%","100%","50%"]} justify="flex-end">
-        <AspectRatio w={["100%","100%","100%","100%","3xl"]} mt={["-10px"]} mb={["10px","20px","30px","30px","50px"]} ratio={[3, 3, 3, 3, 6/4]} >
-            <Image objectPosition={["0 -34px","0 -40px","0 -50px","0 -60px", "0px"]} borderRadius={{md: "0 0 0 70px"}} src="/hero-metronet-image.jpg" alt="Hero Image Metronet" />
+        <Flex w={["100%","100%","100%","100%","50%"]} mt="0 !important" justify="flex-end">
+        <AspectRatio w={["100%","100%","100%","100%","100%"]} mb={["10px","20px","30px","30px","50px"]} ratio={[3, 3, 3, 3, 6/4]} >
+            <Image objectPosition={["0 -34px","0 -40px","0 -50px","0 -60px", "0px"]} borderRadius={{xl: "0 0 0 70px"}} src="/hero-metronet-image.jpg" alt="Hero Image Metronet" />
         </AspectRatio>
         </Flex>
         </Stack>
